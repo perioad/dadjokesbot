b6ab95b9aa38ebf74a4a73aaa3f832c860edb80f
perioad <perioalexander@gmail.com>
2023-10-05 08:53:08 +0300

    sending last joke is done

--- src/lambdas/dadjokesbot/index.ts
+++ src/lambdas/dadjokesbot/index.ts
@@ -1,27 +1,19 @@
 import { bot } from './telegram/telegram.initialization';
 import { webhookCallback } from 'grammy';
 import './telegram/commands';
-import { sendJokes } from './jokes/jokes';
 import { handleError } from '../../core/utils/error-handler.util';
 
 // track anniversaries
+// check status codes in tg bot and aws lambda
 export const handler = async (event: any) => {
   try {
-    const { source } = event;
-
-    if (source === 'aws.events') {
-      console.log('event', event);
-
-      await sendJokes();
-
-      return {
-        statusCode: 200,
-        body: 'OK',
-      };
-    }
-
     return await webhookCallback(bot, 'aws-lambda', 'return')(event);
   } catch (error) {
     await handleError('handler', error);
+
+    return {
+      statusCode: 200,
+      body: 'NOT OK',
+    };
   }
 };
--- src/lambdas/send-jokes/index.ts
+++ src/lambdas/send-jokes/index.ts
@@ -1,18 +1,48 @@
-import { sendMessage } from '../../core/utils/send-message.util';
 import { log } from '../../core/utils/logger.util';
 import { handleError } from '../../core/utils/error-handler.util';
+import { usersDB } from '../../core/database/users-database';
+import {
+  InvokeCommand,
+  InvokeCommandInput,
+  LambdaClient,
+} from '@aws-sdk/client-lambda';
+import { jokesDB } from '../../core/database/jokes-database';
 
 export const handler = async (event: any) => {
   try {
     log('send jokes lambda event: ', JSON.stringify(event));
 
-    await sendMessage(event.id, event.joke.joke);
+    const allActiveUsersIds = await usersDB.getAllActiveUsersIds();
+    const lastJoke = await jokesDB.getLastJoke();
 
+    if (!allActiveUsersIds || !lastJoke) {
+      throw `Send jokes error. allActiveUsersIds: ${allActiveUsersIds}; lastJoke: ${lastJoke}`;
+    }
+
+    const lambdaClient = new LambdaClient({ region: process.env.REGION });
+
+    for (const id of allActiveUsersIds || []) {
+      const params: InvokeCommandInput = {
+        FunctionName: process.env.SENDMESSAGE,
+        InvocationType: 'Event',
+        Payload: JSON.stringify({
+          id,
+          message: lastJoke.joke,
+        }),
+      };
+
+      await lambdaClient.send(new InvokeCommand(params));
+    }
     return {
       statusCode: 200,
       body: 'OK',
     };
   } catch (error) {
     await handleError('send jokes lambda handler', error);
+
+    return {
+      statusCode: 200,
+      body: 'NOT OK',
+    };
   }
 };
