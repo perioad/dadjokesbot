c8327f0f30d933928b12d83fe68f42b26abb81af
perioad <perioalexander@gmail.com>
2023-10-15 21:38:36 +0300

    remove keyboard button on any message

--- src/lambdas/dadjokesbot/telegram/commands/message-text.ts
+++ src/lambdas/dadjokesbot/telegram/commands/message-text.ts
@@ -7,11 +7,14 @@ import { Message, OLD_EXPLAIN_BUTTON } from '../telegram.constants';
 
 bot.on('message:text', async ctx => {
   try {
-    if (ctx.message.text === OLD_EXPLAIN_BUTTON) {
-      await ctx.reply(Message.NewExplain);
-    } else {
-      await ctx.reply(getRandomReply());
-    }
+    const message =
+      ctx.message.text === OLD_EXPLAIN_BUTTON
+        ? Message.NewExplain
+        : getRandomReply();
+
+    await ctx.reply(message, {
+      reply_markup: { remove_keyboard: true },
+    });
 
     await sendMessageToAdmin(
       `User:${NL}${JSON.stringify(ctx.from)}${NL}Writes message:${NL}${
