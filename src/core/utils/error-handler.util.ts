7c60281db51e4013c6f2a6f4335cd8e0e04463c0
Aleks <aleks@Admins-MacBook-Pro-2.local>
2024-12-28 15:38:59 +0100

    feat: serialize error in error handler

--- src/core/utils/error-handler.util.ts
+++ src/core/utils/error-handler.util.ts
@@ -8,10 +8,29 @@ export const handleError = async (
   const title = `ERROR in ${functionName}: `;
 
   if (!error) {
-    console.error(title, 'good luck :)');
-  } else {
-    console.error(title, JSON.stringify(error));
+    console.error(title, 'no error was received, good luck :)');
+
+    return;
   }
 
-  await sendMessageToAdmin(`${title}${NL}${JSON.stringify(error)}`);
+  let errorAsString: string;
+
+  if (typeof error === 'string' || typeof error === 'number') {
+    errorAsString = error.toString();
+  } else {
+    const properties = Object.getOwnPropertyDescriptors(error);
+    const serializedError: Record<string, any> = {};
+
+    for (const key in properties) {
+      serializedError[key] = error[key];
+    }
+
+    delete serializedError.stack;
+
+    errorAsString = JSON.stringify(serializedError, null, 2);
+  }
+
+  console.error(title, errorAsString);
+
+  await sendMessageToAdmin(`${title}${NL}${errorAsString}`);
 };
