706ad386ea34fb6d1e1d69478d829da9947889b6
Aleks <aleks@Admins-MacBook-Pro-2.local>
2025-02-16 16:32:47 +0100

    fix: error handler circular deps error

--- src/core/utils/error-handler.util.ts
+++ src/core/utils/error-handler.util.ts
@@ -1,9 +1,12 @@
 import { sendMessageToAdmin } from './admin-message.util';
+import { log } from './logger.util';
 
 export const handleError = async (
   functionName: string,
   error: any,
 ): Promise<void> => {
+  log('handleError', `ERROR in ${functionName}: `, 'error', error);
+
   const title = `ERROR in ${functionName}: `;
 
   if (!error) {
@@ -17,16 +20,7 @@ export const handleError = async (
   if (typeof error === 'string' || typeof error === 'number') {
     errorAsString = error.toString();
   } else {
-    const properties = Object.getOwnPropertyDescriptors(error);
-    const serializedError: Record<string, any> = {};
-
-    for (const key in properties) {
-      serializedError[key] = error[key];
-    }
-
-    delete serializedError.stack;
-
-    errorAsString = JSON.stringify(serializedError, null, 2);
+    errorAsString = `Error message: ${error?.message}\nError status: ${error?.status}`;
   }
 
   console.error(title, errorAsString);
